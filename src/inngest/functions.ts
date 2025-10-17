30fcfd2fd5aeec353345a22c1f02909faf1977ca
ToufikIIT <toufik.haque2021@gmail.com>
2025-10-18 00:49:41 +0530

    agentic-kit

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,19 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const writer = createAgent({
+      name: "writer",
+      system:
+        "You are an expert writer.  You write readable, concise, simple content.",
+      model: gemini({ model: "gemini-2.5-pro" , apiKey: process.env.GEMINI_KEY! }),
+    });
+    const { output } = await writer.run(
+      `Write about the following topic in 100 words: ${event.data.value}`
+    );
+    return { output };
+  }
 );
