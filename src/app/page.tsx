4c634fb102cca450ba1e7bee38553c76c56ae521
ToufikIIT <toufik.haque2021@gmail.com>
2025-10-17 21:16:51 +0530

    trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import { useTRPC } from '@/trpc/client'
 import React from 'react'
 
 const page = () => {
+  const trpc = useTRPC();
+  trpc.hello.queryOptions({text:"Hello!"})
+
   return (
     <div>
       
