4f9c9c508012df5c986c35ad6c4c3ca3d75cb13e
ToufikIIT <toufik.haque2021@gmail.com>
2025-11-07 12:09:35 +0530

    fragment view and project header

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,44 +1,45 @@
 "use client";
-
-import { useTRPC } from "@/trpc/client";
-import {useSuspenseQuery,} from "@tanstack/react-query";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import MessagesContainer from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
+import ProjectHeader from "../components/project-header";
+import FragmentWeb from "../components/fragment-web";
 
 interface Props {
   projectId: string;
 }
 
 const ProjectView = ({ projectId }: Props) => {
-  /* const trpc = useTRPC();
-  const { data: project } = useSuspenseQuery(
-    trpc.projects.getOne.queryOptions({
-      id: projectId,
-    })
-  ); */
+
+  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+
   return (
     <div className="h-screen">
       <ResizablePanelGroup direction="horizontal">
         <ResizablePanel
-            defaultSize={35}
-            minSize={20}
-            className="flex flex-col min-h-0"
+          defaultSize={35}
+          minSize={20}
+          className="flex flex-col min-h-0"
         >
-            <Suspense fallback={<p>Loading messages...</p>}>
-                <MessagesContainer projectId={projectId} />
-            </Suspense>
+          <Suspense fallback={<p>Loading projects...</p>}>
+            <ProjectHeader projectId={projectId} />
+          </Suspense>
+          <Suspense fallback={<p>Loading messages...</p>}>
+            <MessagesContainer 
+              projectId={projectId} 
+              activeFragment = {activeFragment}
+              setActiveFragment = {setActiveFragment}
+            />
+          </Suspense>
         </ResizablePanel>
-        <ResizableHandle withHandle/>
-        <ResizablePanel
-            defaultSize={65}
-            minSize={50}
-        >
-           
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={65} minSize={50}>
+         {!!activeFragment &&  <FragmentWeb data={activeFragment} />}
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
