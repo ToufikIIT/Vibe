67874bec7bd52198c4fce492d9dd57c5ab8fd2ce
Sk Toufik Haque <toufik.haque2021@gmail.com>
2025-11-13 10:25:10 +0530

    Home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,41 +1,36 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useTRPC } from "@/trpc/client";
-import { useMutation} from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { toast } from "sonner";
+import ProjectForm from '@/modules/home/ui/components/project-form'
+import ProjectsList from '@/modules/home/ui/components/projects-list';
+import Image from 'next/image'
+import React from 'react'
 
 const page = () => {
-  const router = useRouter();
-
-  const [value, setValue] = useState("");
-  const trpc = useTRPC();
-  const createProject = useMutation(
-    trpc.projects.create.mutationOptions({
-      onError: (error) => {
-        toast.error(error.message);
-      },
-      onSuccess: (data) => {
-        router.push(`/projects/${data.id}`);
-      }
-    })
-  );
   return (
-    <div className="h-screen flex items-center justify-center w-screen">
-      <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center">
-        <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button
-        disabled={createProject.isPending}
-        onClick={() => createProject.mutate({ value: value })}
-      >
-        Submit
-      </Button>
-      </div>
+    <div className='flex flex-col max-w-5xl mx-auto w-full'>
+      <section className='space-y-6 py-[16vh] 2xl:py-48'>
+        <div className='flex flex-col items-center'>
+          <Image
+            src="/logo.svg"
+            alt="alt"
+            width={50}
+            height={50}
+            className='hidden md:block'
+          />
+        </div>
+        <h1 className='text-2xl md:text-5xl font-bold text-center'>
+          Build something with Vibe
+        </h1>
+        <p className='text-lg md:text-xl text-muted-foreground text-center'>
+          Create Apps and Websites by chatting with AI.
+        </p>
+        <div className='max-w-3xl mx-auto w-full'>
+          <ProjectForm />
+        </div>
+      </section>
+      <ProjectsList />
     </div>
-  );
-};
+  )
+}
 
-export default page;
+export default page
